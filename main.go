4f58ee3c108c1084c3df878c25f1e63f5523dbcb
Daryn Forman <2016114318@ub.edu.bz>
2025-02-04 17:08:28 -0600

    added a web server

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+func message(w http.ResponseWriter, r *http.Request) {
+	log.Print(w, "Welcome to my Go web server!")
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	log.Print("Hello, World!")
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", message)
+	log.Print("starting server on :4000")
+
+	// start a local web server
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
+
 }
